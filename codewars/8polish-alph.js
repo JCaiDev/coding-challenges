e4175ddd3aa8f241a51366027a04d4f0bc9dfc80
Jackie Dai <jackydai12.21@gmail.com>
2024-02-14 09:45:03 -0500

    cw

---
+++ codewars/8polish-alph.js
@@ -0,0 +1,15 @@
+function correctPolishLetter(str) {
+    let polishLetter = {
+        'ą' : 'a',
+        'ć' : 'c',
+        'ę' : 'e',
+        'ł' : 'l',
+        'ń' : 'n',
+        'ó' : 'o',
+        'ś' : 's',
+        'ź' : 'z',
+        'ż' : 'z'
+    }
+
+    return str.replace(/[ąćęłńńóśźż]/g, match=>polishLetter[match])
+}
