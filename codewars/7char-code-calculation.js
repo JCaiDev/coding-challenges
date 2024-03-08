00cf02b4db73810c98b4c25f74caf4201c00334e
Jackie Dai <jackydai12.21@gmail.com>
2024-03-08 02:47:43 -0500

    cw

---
+++ codewars/7char-code-calculation.js
@@ -0,0 +1,8 @@
+function calc(x) {
+   const sum = n => [...n].reduce((a, b)=> +a + +b)
+   
+   let total1 = x.replace(/./g, x => x.charCodeAt(0))
+   let total2 = total1.replace(/7/g, '1')
+
+   return sum(total1)-sum(total2)
+}
