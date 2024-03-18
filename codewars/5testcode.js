25381c6906b655ea0b6571a9138ffd13612d5003
Jackie Dai <jackydai12.21@gmail.com>
2024-03-18 09:30:08 -0400

    cw

--- codewars/5testcode.js
+++
@@ -1,4 +0,0 @@
-function test(secondtime){
-    let today = 0
-    if today < 2 return secondtime 
-}
---
+++ codewars/7prepare-the-count-of-languages.js
@@ -0,0 +1,6 @@
+function countLanguages(list) {
+    let count = {}
+    list.forEach(x => count[x.language] = (count[x.language] || 0) + 1)
+
+    return count
+}
