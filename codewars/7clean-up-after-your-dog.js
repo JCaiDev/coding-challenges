69d39d8a54adfa6c6a1380353f4819b00d9bac42
Jackie Dai <jackydai12.21@gmail.com>
2024-03-25 16:45:42 -0400

    cw

---
+++ codewars/7clean-up-after-your-dog.js
@@ -0,0 +1,5 @@
+function crap(x, bag, cap) {
+    let yard = x.reduce((a, b)=> a.concat.b)
+
+    return yard.includes('D') ? 'Dog!' : bag*cap - x.filter(v=> v === '@').length > 0 ? 'Clean' : "Crap"
+}
