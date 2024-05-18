ba49c8848a59ae0a9a063d503bf909e984b23b5c
Jackie Dai <jackydai12.21@gmail.com>
2024-05-18 19:38:58 -0400

    cw

--- codewars/pigLatin.js
+++ codewars/pigLatin.js
@@ -1,21 +1,5 @@
 function pigLatin(str) {
-    str = str.toLowerCase()
-
-    const vowels = ['a', 'e', 'i' , 'o' , 'u']
-    
-    let vowelIndex = 0
-
-    if (vowels.includes(str[0])) {
-        return str + 'way'
-    } else {
-        for (let char of str) {
-            if (vowels.includes(char)) {
-                vowelIndex = str.indexOf(char)
-                break
-            }
-        }
-        return str.slice(vowelIndex) + str.slice(0, vowelIndex) + 'ay'
-    }
-
-
+    return str
+        .replace(/^([aeiouy])(.*)/, $1$2way)
+        .replace(/^([^aeiougy])+(.*)/,$2$1ay)
 }
