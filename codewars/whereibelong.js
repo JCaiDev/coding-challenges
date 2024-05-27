f2d1fa58f751d2d0010e6047bbd4b2d17e693cf5
Jackie Dai <jackydai12.21@gmail.com>
2024-05-27 17:29:30 -0400

    cw

---
+++ codewars/whereibelong.js
@@ -0,0 +1,11 @@
+function whereIBelong(arr, num) {
+    let counter = 0
+
+    for (let index = 0; index < array.length; index++) {
+        if (num > arr[i]) {
+            counter++
+        }
+        
+    }
+    return counter
+}
