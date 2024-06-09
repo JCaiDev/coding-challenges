fbd3fafd44f9d09878b4341f1fe14a8606ead077
Jackie Dai <jackydai12.21@gmail.com>
2024-06-09 12:00:59 -0400

    cw

--- codewars/7who-is-online.js
+++ codewars/7who-is-online.js
@@ -5,7 +5,6 @@ function whoseOnline (friends) {
         if(status === 'online' && friend.lastActivity > 10 ) {
             status = 'away'
         }
-        let temp = output[status];
 
         if (output[status]) {
             output[status].push(friend.username)
