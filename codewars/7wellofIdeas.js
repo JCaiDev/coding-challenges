a7ccde42515421b79cfda827708af08409e8f171
Jackie Dai <jackydai12.21@gmail.com>
2024-06-23 16:29:40 -0400

    cw

---
+++ codewars/7wellofIdeas.js
@@ -0,0 +1,8 @@
+function well(x) {
+    let match = ("" + x).match(/good/gi)
+    let goodCount = match.length
+
+    return goodCount > 2 ? "I smell a series!" : goodCount > 0 ? "Publish!" : 'Failed!'
+
+
+}
