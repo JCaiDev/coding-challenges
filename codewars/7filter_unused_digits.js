bad1d6bf6b0e4972f0ab4a8387038912dfee9ee9
Jackie Dai <jackydai12.21@gmail.com>
2024-06-27 10:37:22 -0400

    cw

--- codewars/7filter_unused_digits.js
+++ codewars/7filter_unused_digits.js
@@ -3,4 +3,8 @@ function unusedDigits(...args) {
 
     return [0,1,2,3,4,5,6,7,8,9].filter(x=> !digits.includes(x)).join('')
 
-} 
+}
+
+function unusedDigits2(...args) {
+    return "0123456789".replace(new RegExp('[' + args.join('') + ']', 'g'), '')
+}
