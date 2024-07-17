33a4ccc1f80e76fdb5955dbccb1030000faa077c
Jackie Dai <jackydai12.21@gmail.com>
2024-07-17 11:26:54 -0400

    cw

---
+++ codewars/7mergeArrays.js
@@ -0,0 +1,10 @@
+function mergeArrays(a, b) {
+    let result = []
+    let maxLength = Math.max(a.length, b.length)
+
+    for (let i = 0; i < maxLength; i++) {
+        if (i<a.length) result.push(a[i])
+        if (i<b.length) result.push(b[i])
+    }
+    return result
+}
