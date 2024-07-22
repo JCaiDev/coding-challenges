f0402f15ea915a20d1a6a75cc2bcd53473acb7b7
Jackie Dai <jackydai12.21@gmail.com>
2024-07-22 14:01:43 -0400

    lc

---
+++ codewars/LC34.ts
@@ -0,0 +1,37 @@
+// must write O(log n) time complexity solution
+
+function searchRange(nums: number[], target: number): [number, number] {
+  const binarySearch = (
+    nums: number[],
+    target: number,
+    isSearchingLeft: boolean
+  ) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let idx = -1;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (nums[mid] > target) {
+        right = mid - 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        idx = mid;
+        if (isSearchingLeft) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+
+    return idx;
+  };
+
+  const left = binarySearch(nums, target, true);
+  const right = binarySearch(nums, target, false);
+
+  return [left, right];
+}
