2597e376dcd612f9be7a28b0b20294d5dc132a74
Jackie Dai <jackydai12.21@gmail.com>
2024-07-25 14:51:14 -0400

    cw

--- codewars/binarySearch.ts
+++ codewars/binarySearch.ts
@@ -8,3 +8,17 @@
 // find new mid point - repeat until mid point = target
 // if cannot find value return -1
 // return mid, which should be the index of the target
+
+function binarySearchTS(arr: number[], elem: number): number {
+  let start: number = 0;
+  let end: number = arr.length - 1;
+  let mid: number = Math.floor((start + end) / 2);
+  while (start <= end) {
+    if (elem < arr[mid]) end = mid - 1;
+    else start = mid + 1;
+    mid = Math.floor((start + end) / 2);
+  }
+  return arr[mid] === elem ? mid : -1;
+}
+
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15));
