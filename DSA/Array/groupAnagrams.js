ff27405812c529c2907ddc780ad1e09fc955ca6e
Jackie Dai <jackydai12.21@gmail.com>
2024-11-04 10:06:02 -0500

    cw

--- DSA/Array/groupAnagrams.js
+++ DSA/Array/groupAnagrams.js
@@ -9,4 +9,21 @@
 //   return Object.values(cache);
 // }
 
+function groupAnagrams(strs) {
+  let cache = {};
 
+  for (let str of strs) {
+    const count = new Array(26).fill(0);
+
+    for (let c of str) {
+      count[c.charCodeAt() - "a".charCodeAt()] += 1;
+    }
+    const key = count.join("");
+
+    if (!cache[key]) {
+      cache[key] = [];
+    }
+    cache[key].push(str);
+  }
+  return Object.values(cache);
+}
