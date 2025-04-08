e534fb7b73bb0a25daaca715418770fc1561c864
Jackie Dai <jackydai12.21@gmail.com>
2025-04-08 01:10:32 -0400

    dsa

---
+++ DSA/Trees/BST.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+    }
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+tree.root.left.left = new Node(5);
